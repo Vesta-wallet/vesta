91d3eb7cf8f673ecc6e945e54c4e4c411d50c50f
Jack <jack.day@protonmail.com>
2020-12-16 19:31:57 -0700

    Added bottom nav bar skeleton

--- vesta/lib/main.dart
+++ vesta/lib/main.dart
@@ -1,24 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Vesta',
-      home: Scaffold(
-        backgroundColor: Color(0xff141414),
-        body: Center(child: Body()),
-      ),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
 }
 
-/// This is the stateless widget that the main application instantiates.
-class Body extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w500);
+  final List<Widget> _children = [
+    Text(
+      "Home",
+      style: optionStyle,
+    ),
+    Text(
+      "Messages",
+      style: optionStyle,
+    ),
+    Text(
+      "Profile",
+      style: optionStyle,
+    )
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('How Are You?');
+    return Scaffold(
+      body: Center(child: _children[_currentIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
   }
 }
