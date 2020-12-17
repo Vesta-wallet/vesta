d8ea19bd9959e362b82f4b68c9671582ab741f79
Jack <jack.day@protonmail.com>
2020-12-16 17:43:46 -0700

    Changed background to dark theme

--- vesta/lib/main.dart
+++ vesta/lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(home: new HomePage()));
-}
+void main() => runApp(MyApp());
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Center(
-        child: new Text("Nothing"),
+    return MaterialApp(
+      title: 'Vesta',
+      home: Scaffold(
+        backgroundColor: Color(0xff141414),
+        body: Center(child: Body()),
       ),
     );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('How Are You?');
   }
 }
